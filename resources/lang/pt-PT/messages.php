a53954a8133b543ec7b6ba7f0dc5731631887cfe
Minat0_ <42711312+Tomut0@users.noreply.github.com>
2021-04-26 16:31:22 +0300

    New translations messages.php (Portuguese)

--- resources/lang/pt-PT/messages.php
+++ resources/lang/pt-PT/messages.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-    'header.home' => 'Home',
-    'header.clans' => 'Clans',
-    'header.players' => 'Players',
-    'main.clanName' => 'Name',
-    'main.clanKDR' => 'Kills/Deaths',
-    'main.clanMembers' => 'Members',
-    'main.playerName' => 'Nickname',
-    'main.playerKDR' => 'Kills/Deaths',
-    'main.victim' => 'Victim',
-    'main.attacker' => 'Attacker',
-    'clans.clanTag' => 'Tag',
-    'clans.clanLeaders' => 'Leaders',
-    'clans.clanFounded' => 'Founded',
-    'clans.clanLastUsed' => 'Last Used',
-    'detail.error' => 'Error',
-    'detail.dataNotFound' => 'Data not found'
+    'header.home' => '',
+    'header.clans' => '',
+    'header.players' => '',
+    'main.clanName' => '',
+    'main.clanKDR' => '',
+    'main.clanMembers' => '',
+    'main.playerName' => '',
+    'main.playerKDR' => '',
+    'main.victim' => '',
+    'main.attacker' => '',
+    'clans.clanTag' => '',
+    'clans.clanLeaders' => '',
+    'clans.clanFounded' => '',
+    'clans.clanLastUsed' => '',
+    'detail.error' => '',
+    'detail.dataNotFound' => ''
 ];
