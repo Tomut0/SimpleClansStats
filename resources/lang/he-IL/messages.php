22717fa8a44c03f99c940c09db1d3167c4d1ba6a
Minat0_ <42711312+Tomut0@users.noreply.github.com>
2021-04-26 05:01:05 +0300

    New translations messages.php (Hebrew)

---
+++ resources/lang/he-IL/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'header.home' => 'Home',
+    'header.clans' => 'Clans',
+    'header.players' => 'Players',
+    'main.clanName' => 'Name',
+    'main.clanKDR' => 'Kills/Deaths',
+    'main.clanMembers' => 'Members',
+    'main.playerName' => 'Nickname',
+    'main.playerKDR' => 'Kills/Deaths',
+    'main.victim' => 'Victim',
+    'main.attacker' => 'Attacker',
+    'clans.clanTag' => 'Tag',
+    'clans.clanLeaders' => 'Leaders',
+    'clans.clanFounded' => 'Founded',
+    'clans.clanLastUsed' => 'Last Used',
+    'detail.error' => 'Error',
+    'detail.dataNotFound' => 'Data not found'
+];
