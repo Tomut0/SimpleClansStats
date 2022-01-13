74b3f6ec2d09bcd95f2effbdad61bc058481fc19
Minat0_ <minat0_@outlook.com>
2022-01-14 02:09:50 +0300

    Add Language-selector and Language options to environment variables.

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,23 @@
 |
 */
 
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/clans', 'ViewController@show')->name('clans');
-Route::get('/{locale}/clans', 'ViewController@show')->name('clans');
-Route::get('/detail/clan/{tag}', 'ViewController@detail')->name('clan');
-Route::get('/detail/player/{name}', 'ViewController@detail')->name('player');
+//Route::get('/detail/clan/{tag}', 'ViewController@detail')->name('clan');
+//Route::get('/detail/player/{name}', 'ViewController@detail')->name('player');
+
+$LOCALE_REGEX = '^[A-Za-z]{2,4}([_-][A-Za-z]{4})?([_-]([A-Za-z]{2}|[0-9]{3}))?$';
+
 Route::get('/', 'ViewController@show');
-Route::get('/{locale}', 'ViewController@show');
+Route::get('/clans', 'ViewController@show')->name('clans');
+
+if (Env::get('LANGUAGE_SELECTOR')) {
+    Route::group([
+        'prefix' => '{locale}',
+        'where' => ['locale' => $LOCALE_REGEX]
+    ], function () {
+        Route::get('/', 'ViewController@show');
+        Route::get('/clans', 'ViewController@show')->name('clans');
+    });
+}
