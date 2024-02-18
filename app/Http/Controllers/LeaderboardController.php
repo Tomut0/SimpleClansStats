6bcb162f8e7c7a2d6f402be0cf08c6fb95315b69
Tomut0 <minat0_@outlook.com>
2024-02-18 21:27:54 +0300

    extract compare positions to a method, add validation

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -20,37 +20,65 @@ class LeaderboardController extends Controller
 {
     public function index(Request $request): Response|ResponseFactory
     {
+        $validatedData = $request->validate([
+            'sortBy' => ['bail', 'nullable', 'string'],
+            'period' => ['nullable', 'string'],
+            'limit' => ['nullable', 'integer', 'min:1', 'max:100'],
+        ]);
+
         $selectors = [
             'sortSelector' => SortTypes::packed(),
             'intervalSelector' => Period::packed(),
         ];
 
         $sortKeys = array_keys(SortTypes::packed());
-        $sortBy = $request->input('sortBy', $sortKeys[0]);
+        $sortBy = $validatedData['sortBy'] ?? $sortKeys[0];
 
         try {
-            $period = constant("App\Enums\Period::" . ucfirst($request->input('period')));
+            $period = constant("App\Enums\Period::" . ucfirst($validatedData['period'] ?? ""));
         } catch (Error $ignored) {
             $period = Period::from(Period::cases()[0]->value);
         }
 
-        $limit = $request->input('limit', 10);
+        $limit = $request->query('limit', 10);
+
+        // Sort data by specified sorting criteria
         $data = Clan::data();
-
-        // Sort and cache the data for the specified period
         $sortedData = $this->sortTopAll($data, $sortKeys);
-        $cachedPastPositions = Cache::remember($period->name, now()->addDays($period->value), function () use ($sortedData) {
-            return collect($sortedData)->toJson();
-        });
 
-        // todo: extract to a new method (comparePositions, calculatePositions)
-        // Retrieve past positions from cache
-        $pastPositions = json_decode($cachedPastPositions, true);
+        // Initial cache over all periods
+        foreach (Period::cases() as $p) {
+            Cache::add($p->name, $sortedData, now()->addDays($p->value));
+        }
+
+        // Get past positions from cache
+        $pastPositions = Cache::get($period->name);
 
         // Calculate new positions based on the specified sorting criteria
         $newPositions = $data->sortByDesc($sortBy)->values()->take($limit)->toArray();
 
         // Compare positions between past and new positions
+        $comparedPositions = $this->comparePositions($sortBy, $pastPositions, $newPositions);
+
+        return inertia('Dashboard', ['clans' => $comparedPositions, 'selectors' => $selectors]);
+    }
+
+    /**
+     * Compare positions between past and new positions.
+     *
+     * <p>
+     * If the position is the same, set the position_status to "equal".
+     * If the position is lowered, set the position_status to "lowered".
+     * If the position is raised, set the position_status to "raised".
+     * </p>
+     *
+     * @param string $sortBy
+     * @param array $pastPositions
+     * @param array $newPositions
+     * @return array
+     */
+    private function comparePositions(string $sortBy, array $pastPositions, array $newPositions): array
+    {
         foreach ($pastPositions as $pastIndx => $pastPosition) {
             $newIndex = array_search($pastPosition[$sortBy]['tag'], array_column($newPositions, 'tag'));
 
@@ -63,10 +91,10 @@ class LeaderboardController extends Controller
             }
         }
 
-        return inertia('Dashboard', ['clans' => $newPositions, 'selectors' => $selectors]);
+        return $newPositions;
     }
 
-    function sortTopAll(Collection $clans, array $sortBy, int $limit = 10): array
+    private function sortTopAll(Collection $clans, array $sortBy, int $limit = 10): array
     {
         $sortedData = [];
         foreach ($sortBy as $sortType) {
@@ -76,12 +104,12 @@ class LeaderboardController extends Controller
         return $sortedData;
     }
 
-    function sortTop(array &$accumulator, Collection $clans, string $sortBy, int $limit = 10): void
+    private function sortTop(array &$accumulator, Collection $clans, string $sortBy, int $limit = 10): void
     {
         $sorted = $clans->sortByDesc($sortBy)->values()->take($limit)->all();
 
         foreach ($sorted as $position => $clan) {
-            $accumulator[$position][$sortBy] = $clan;
+            $accumulator[$position][$sortBy] = $clan->toArray();
         }
     }
 }
