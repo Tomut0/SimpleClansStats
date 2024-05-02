1cd30c2acde1b7697ee5a862449ccb489cce7b90
Tomut0 <minat0_@outlook.com>
2024-05-02 14:47:50 +0300

    fix: avoid stackoverflow on translations

--- resources/js/trans.js
+++ resources/js/trans.js
@@ -1,6 +1,15 @@
 import {usePage} from "@inertiajs/vue3";
 
+
+function isFallback(locale) {
+    return locale === usePage().props.fallback_locale;
+}
+
 export function __(key, replacement = {}, locale = usePage().props.locale) {
+    if (import.meta.env.MODE === 'development') {
+        console.log('Loaded translation key:', key);
+    }
+
     if (!key) {
         throw new Error("Translation key is undefined or null");
     }
@@ -8,8 +17,12 @@ export function __(key, replacement = {}, locale = usePage().props.locale) {
     const translations = JSON.parse(usePage().props.translations);
 
     let message = key.split('.').reduce((t, i) => (t && t[i]) ?
-        t[i] : __(key, replacement, usePage().props.fallback_locale), translations[locale]);
+        t[i] : isFallback(locale) ? false : __(key, replacement, usePage().props.fallback_locale), translations[locale]);
 
+    if (!message) {
+        console.warn(`Translation key [${key}] wasn't found`);
+        return key;
+    }
 
     Object.keys(replacement).forEach(key => {
         message = message.replace(`:${key}`, replacement[key]);
