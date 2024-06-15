221eeea2066e07cf1b55212e2d75b7d19fb46e24
Tomut0 <minat0_@outlook.com>
2024-06-16 00:06:53 +0300

    add bakeIcons and formatNumber functions to helpers

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -1,3 +1,5 @@
+import {defineAsyncComponent} from "vue";
+
 function debounce(func, delay) {
     let timerId;
 
@@ -12,10 +14,42 @@ function debounce(func, delay) {
     };
 }
 
+/**
+ * Get the value of a query parameter from a URL
+ *
+ * @param url
+ * @param key
+ * @returns {string}
+ */
 function queryValue(url, key) {
     const queryString = url.split('?')[1];
     const params = new URLSearchParams(queryString);
     return params.get(key);
+}
+
+function formatNumber(num, locale = 'en-US', style = 'currency', minimumFractionDigits = 0, maximumFractionDigits = 1) {
+    const formatter = new Intl.NumberFormat(locale, {
+        style: style,
+        notation: 'compact',
+        currency: 'USD', // this is the fallback value
+        compactDisplay: 'short',
+        minimumFractionDigits: minimumFractionDigits,
+        maximumFractionDigits: maximumFractionDigits
+    });
+
+    return formatter.format(num);
+}
+
+/**
+ * Bake an icon component from the Heroicons Vue library
+ *
+ * @see https://heroicons.com/
+ * @param iconName
+ */
+function bakeIcon(iconName) {
+    return defineAsyncComponent(() => {
+        return import(`../../node_modules/@heroicons/vue/24/solid/esm/${iconName}.js`);
+    })
 }
 
 function omit(obj, keys) {
@@ -27,4 +61,4 @@ function omit(obj, keys) {
     });
 }
 
-export {debounce, queryValue};
+export {debounce, queryValue, formatNumber, bakeIcon};
