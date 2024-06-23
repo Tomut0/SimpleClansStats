dcba8a903c3f878a1148b102f3a9b4426fb23e43
Tomut0 <minat0_@outlook.com>
2024-06-23 23:28:20 +0300

    it's time to collect chart data :sunglasses:

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Enums\Period;
+use App\Utils;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +14,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('scstats:collect-monthly')->weekly();
+        $schedule->command('scstats:collect-weekly')->daily();
+        $schedule->command('scstats:collect-daily')->hourly();
     }
 
     /**
--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\Period;
+use App\Utils;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +19,18 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('scstats:collect-daily', function () {
+    Utils::saveStatistics(Period::Daily);
+    $this->comment('Daily statistics collected.');
+})->purpose('Makes a snapshot of data from the database and stores it in the cache.');
+
+Artisan::command('scstats:collect-weekly', function () {
+    Utils::saveStatistics(Period::Weekly);
+    $this->comment('Weekly statistics collected.');
+})->purpose('Makes a snapshot of data from the database and stores it in the cache.');
+
+Artisan::command('scstats:collect-monthly', function () {
+    Utils::saveStatistics(Period::Monthly);
+    $this->comment('Monthly statistics collected.');
+})->purpose('Makes a snapshot of data from the database and stores it in the cache.');
