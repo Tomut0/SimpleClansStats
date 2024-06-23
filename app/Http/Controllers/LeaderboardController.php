3677b3da34da48e76cc9f13a7255c88849368a66
Tomut0 <minat0_@outlook.com>
2024-06-23 23:33:23 +0300

    add logic to last kills and charts

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Enums\Period;
 use App\Enums\SortTypes;
 use App\Models\Clan;
+use App\Models\Kill;
+use Carbon\Carbon;
 use Error;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
@@ -48,8 +50,12 @@ class LeaderboardController extends Controller
         }
 
         $currentSelectors = [
-            'sortSelector' => $selectors['sortSelector'][$sortBy],
-            'intervalSelector' => $selectors['intervalSelector'][strtolower($period->name)],
+            'sortSelector' => [
+                $sortBy => $selectors['sortSelector'][$sortBy]
+            ],
+            'intervalSelector' => [
+                strtolower($period->name) => $selectors['intervalSelector'][strtolower($period->name)]
+            ],
         ];
 
         $limit = $request->query('limit', 10);
@@ -60,11 +66,12 @@ class LeaderboardController extends Controller
 
         // Initial cache over all periods
         foreach (Period::cases() as $p) {
-            Cache::add($p->name, $sortedData, now()->addDays($p->value));
+            $ttl = now()->addDays($p->value);
+            Cache::add($p->name, ["positions" => $sortedData, "ttl" => $ttl], $ttl);
         }
 
         // Get past positions from cache
-        $pastPositions = Cache::get($period->name);
+        $pastPositions = Cache::get($period->name)["positions"];
 
         // Calculate new positions based on the specified sorting criteria
         $newPositions = $data->sortByDesc($sortBy)->values()->take($limit)->toArray();
@@ -72,7 +79,40 @@ class LeaderboardController extends Controller
         // Compare positions between past and new positions
         $comparedPositions = $this->comparePositions($sortBy, $pastPositions, $newPositions);
 
-        return inertia('Dashboard', ['clans' => $comparedPositions, 'selectors' => ['current' => $currentSelectors, 'all' => $selectors]]);
+        // Last 10 kills
+        $lastKills = Kill::with('victimClan', 'attackerClan')->get(['attacker', 'attacker_tag', 'victim', 'victim_tag'])
+            ->where('attacker_tag', '!=', '')->where('victim_tag', '!=', '')
+            ->reverse()->take($limit)->map(function (Kill $kill) {
+                if (!$kill->attackerClan || !$kill->victimClan) return null;
+
+                return [
+                    'attacker' => [
+                        'name' => $kill->attacker,
+                        'tag' => $kill->attackerClan->color_tag,
+                    ],
+                    'victim' => [
+                        'name' => $kill->victim,
+                        'tag' => $kill->victimClan->color_tag,
+                    ]
+                ];
+            })->filter(fn($kill) => !is_null($kill))->values()->toArray();
+
+        // Retrieve kills by type for the specified period
+        $killsByType = Kill::whereNotNull('created_at')->get()
+            ->filter(function (Kill $kill) use ($period) {
+                return Carbon::make($kill->created_at)->greaterThanOrEqualTo(now()->subDays($period->value));
+            })->map(function (Kill $kill) {
+                return $kill->displayType();
+            });
+
+
+        // Retrieve statistics for charts
+        $statistics = Cache::get($period->name)["statistics"] ?? [];
+        $statistics['kills'] = $killsByType->countBy()->toArray();
+
+        return inertia('Dashboard', ['clans' => $comparedPositions, 'lastKills' => $lastKills,
+            'selectors' => ['current' => $currentSelectors, 'all' => $selectors], 'statistics' => $statistics
+        ]);
     }
 
     /**
