db0a0ce2c7ceb219ca0d6da0215ae4a30135ea08
Tomut0 <minat0_@outlook.com>
2024-10-14 03:27:42 +0300

    feat: add og tags and image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,14 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Open Graph Meta Tags -->
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="{{ config('app.name', 'Laravel') }}">
+        <meta property="og:locale" content="{{ str_replace('_', '-', app()->getLocale()) }}">
+        <meta property="og:image" content="/images/og_image.png">
+        <meta property="og:image:width" content="512">
+        <meta property="og:image:height" content="512">
+
         <!-- Scripts -->
         @routes
         @if(env('APP_ENV') === 'production')
